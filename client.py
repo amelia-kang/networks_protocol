13215dc2c4f25cf559fef29061521a15a5c6ee21
Amelia Kang <ameliakang0827@gmail.com>
2020-01-30 21:10:53 -0500

    modulized code

--- client.py
+++ client.py
@@ -5,12 +5,94 @@ import sys
 AVAIL_REQS = ['CONNECT', 'DISCONNECT', 'POST', 'GET', 'PIN', 'UNPIN', 'CLEAR']
 print('Available commands:', AVAIL_REQS)
 
+def connect(client, request, request_code, is_connected):
+    try:
+        if len(request) == 1:
+            server_info = input(
+                'Example: localhost 9090\nEnter server IP and port separated by space: ').split()
+        else:
+            server_info = request[1:]
+        print((server_info[0],server_info[1]))
+        client.connect((server_info[0], int(server_info[1])))
+    except Exception as e:
+        raise Exception('Connection cannot be established. \
+        Please check server input or connection status and retry.\nException: {}'.format(e))
+    is_connected = True
+    client.send(request_code.encode())
+    print('Request {} sent to server, waiting for response...'.format(request))
+    response = client.recv(1024)
+    print('Server response: ', response.decode())
+    return client, is_connected, request
+
+def disconnect(client, request, request_code, is_connected):
+    client.send(request_code.encode())
+    client.close()
+    is_connected = False
+    print('Connection closed.')
+    return client, is_connected
+        
+def post(request, request_code):
+    # If length of request is 1, hint user to keep inputting
+    if len(request) == 1:
+        request_content = input('request requires: coordinates, width, height, color, "message" and status, separated by space.\
+                \nExample: 6 6 5 5 red Pick up Fred from home at 5 pinned\
+                \nEnter your POST message: ')
+        if request_content == '':
+            print('No input provided. Please retry.')
+            return None
+        request_split = request_content.split()
+        request = request[0] + ' ' + request_content
+    else:
+        request_split = request[1:]
+        request = request_raw
+        if len(request_split) < 5:
+            print('Insufficient params in the request. Please try again.')
+            return None
+    # Validate if first 4 inputs are numbers
+    if not request_split[0].isdigit() or not request_split[1].isdigit() or not request_split[2].isdigit() or not request_split[3].isdigit():
+        print('Invalid request input: numbers are required for first 4 inputs. Please try again.')
+        return None
+    return request
+
+
+def get(request, request_code, request_raw):
+    if len(request)==1:
+        get_param = input(
+            'Example 1 - color=red contains=4,6 refersTo=Fred, each separated by space\
+                \nExample 2 - PINS or ALL\
+                \nYour GET param: ')
+        if get_param == '':
+            print('No input provided. Please retry.')
+            return None
+        request = request_code + ' ' + get_param
+    else:
+        request = request_raw
+    return request
+
+
+def pin_unpin(request, request_code, request_raw):
+    if len(request)==1:
+        coords = input('Example: 7,7\nEnter a pair of coordinate separated by comma: ')
+        coords_list = coords.split() 
+        request = request_code+' '+coords
+    elif len(request) ==2: 
+        coords_list = request[1].split(',')
+        request = request_raw
+    else:
+        print('Invalid format for this PIN/UNPIN request. Example: PIN 6,7. Please try again.')
+        return None
+    # Validate if the coordinates are numbers
+    if len(coords_list) != 2 or not coords_list[0].isdigit() or not coords_list[1].isdigit():
+        print('Invalid coordinates entered. Please retry and enter 2 digits.')
+        return None 
+    return request
+
+
 # Define TCP socket
 client = socket(AF_INET, SOCK_STREAM)
 is_connected = False
 
 while True:
-
     # Get capitalized user input
     request_raw = input('Input a request: ').strip()
     request = request_raw.split()
@@ -18,96 +100,35 @@ while True:
     if request_code not in AVAIL_REQS:
         print('Given request is not valid. Please check valid options and retry.')
         continue
-
     try:
         if request_code == 'CONNECT':
             if is_connected == True:
-                print('Client already connected.')
+                print('Client already connected to a server {}'.format)
                 continue
-            try:
-                if len(request) == 1:
-                    server_info = input(
-                        'Enter server IP and port separated by space: ').split()
-                else:
-                    server_info = request[1:]
-                print((server_info[0],server_info[1]))
-                client.connect((server_info[0], int(server_info[1])))
-            except Exception as e:
-                raise Exception('Connection cannot be established. \
-                Please check server input or connection status and retry.\nException: {}'.format(e))
-            is_connected = True
-            client.send(request_code.encode())
-            print('Request {} sent to server, waiting for response...'.format(request))
-            response = client.recv(1024)
-            print('Server response: ', response.decode())
+            client, is_connected, request = connect(client, request, request_code, is_connected)
 
         elif request_code == 'DISCONNECT':
             if is_connected == False:
                 print('Client has not opened a connection or connection already closed.')
             else:
-                client.send(request_code.encode())
-                client.close()
-                is_connected = False
-                print('Connection closed.')
+                client, is_connected = disconnect(client, request, request_code, is_connected)
             break
-
         else:
             if is_connected == False:
                 print('No open connection established. Please CONNECT to server first.')
                 continue
-
             if request_code == 'POST':
-                # If length of request is 1, hint user to keep inputting
-                if len(request) == 1:
-                    request_content = input('request requires: coordinates, width, height, color, "message" and status, separated by space.\
-                            \nExample: 6 6 5 5 red Pick up Fred from home at 5 pinned\
-                            \nEnter your POST message: ')
-                    if request_content == '':
-                        print('No input provided. Please retry.')
-                        continue
-                    request_split = request_content.split()
-                    request = request[0] + ' ' + request_content
-                else:
-                    request_split = request[1:]
-                    request = request_raw
-                # Validate if first 4 inputs are numbers
-                if not request_split[0].isdigit() or not request_split[1].isdigit() or not request_split[2].isdigit() or not request_split[3].isdigit():
-                    print(
-                        'Invalid request input: numbers are required for first 4 inputs. Please try again.')
-                    continue
-                
+                request = post(request, request_code)
+                if request == None:
+                    continue  
             elif request_code == 'GET':
-                if len(request)==1:
-                    get_param = input(
-                        'Example 1 - color=red contains=4,6 refersTo=Fred, each separated by space\
-                            \nExample 2 - PINS or ALL\
-                            \nYour GET param: ')
-                    if get_param == '':
-                        print('No input provided. Please retry.')
-                        continue
-                    request = request_code + ' ' + get_param
-                else:
-                    request = request_raw
-
-            elif request_code == 'PIN' or request_code=='UNPIN':
-                if len(request)==1:
-                    coords = input('Example: 7,7'\
-                        'Enter a pair of coordinate separated by comma: ')
-                    coords_list = coords.split() 
-                    request = request_code+' '+coords
-                elif len(request) ==2: 
-                    coords_list = request[1].split(',')
-                    request = request_raw
-                else:
-                    print('Invalid format for this PIN/UNPIN request. Please try again.')
+                request = get(request, request_code, request_raw)
+                if not request:
                     continue
-                # Validate if the coordinates are numbers
-                if len(coords_list) != 2 or not coords_list[0].isdigit() or not coords_list[1].isdigit():
-                    print(
-                        'Invalid coordinates entered. Please retry and enter 2 digits.')
-                    continue 
-
-
+            elif request_code == 'PIN' or request_code=='UNPIN':
+                request = pin_unpin(request, request_code, request_raw)
+                if not request:
+                    continue
             elif request_code == 'CLEAR':
                 request = request_code
 
@@ -119,5 +140,5 @@ while True:
             print('Server response:\n{}'.format(response.decode()))
 
     except Exception as e:
-        print(e)
+        print('Exception occurred: {}\nPlease follow instructions and try again.'.format(e))
         continue
--- server.py
+++ server.py
@@ -6,6 +6,153 @@ from time import sleep
 DEFAULT_PIN_STATUS = False
 GET_REQ_PARAMS = ['COLOR','CONTAINS','REFERSTO']
 
+
+# Form connect server response to send back to client
+def server_connect():
+    response = 'Available colors for notes: ' + \
+            str(AVAIL_COLORS) + '\nBoard size: ' + \
+            str((BOARD_WIDTH, BOARD_HEIGHT))
+    return response
+
+
+# Clear any unpinned notes, and form response message for client
+# param: global notes
+def server_clear(notes):
+    init_len = len(notes)
+    notes = list(filter(lambda n: n['is_pinned']==True, notes))
+    response = "{} unpinned note(s) removed".format(init_len-len(notes))
+    return response, notes
+
+
+# PIN or UNPIN message depending on the request code, then form response
+def server_pin_unpin(request, request_code):
+    pin_count = 0
+    response = ''
+    if len(request)==2 and ',' in request[1]:
+        coords = request[1].split(',')
+        x,y = int(coords[0]), int(coords[1])
+        # Loop through each note
+        for note in notes:
+            # Find maching coordinates
+            if note['x']==x and note['y']==y:
+                # If asked to PIN, pin
+                if request_code == 'PIN':
+                    if note['is_pinned'] == True:
+                        response += 'Note at {},{} already pinned'.format(x,y)
+                    else:
+                        note['is_pinned'] = True
+                        pin_count+=1
+                # If asked to unpin, unpin
+                else:
+                    if note['is_pinned'] == False:
+                        response += 'Note at {},{} already not pinned'.format(x,y)
+                    else:
+                        note['is_pinned'] = False
+                        pin_count+=1
+        response += '{} note(s) {}.'.format(pin_count, 'pinned' if request_code=='PIN' else 'unpinned')
+        return response
+
+
+# Add the post to note board, update notes list, and return a response and notes
+def server_post(request, notes):
+    response = ''
+    try:
+        x = int(request[1])
+        y = int(request[2])
+        width = int(request[3])
+        height = int(request[4])
+        if x<0 or y<0 or y>BOARD_HEIGHT or width > BOARD_WIDTH or height>BOARD_HEIGHT:
+            raise Exception("Note does not fit on the board. Please resize/re-position to fit it in {}x{}.".format(BOARD_WIDTH, BOARD_HEIGHT))
+
+        color = request[5]
+        # If the value at index 5 is not in available color list, set it to None
+        if color.upper() not in AVAIL_COLORS:
+            color = None
+        # If status is not pinned, set is_pinned to False
+        status = request[-1].upper()
+        is_pinned = True if status == 'PINNED' else False
+
+        if color and is_pinned:
+            message = ' '.join(request[6:-1])
+        elif color and not is_pinned:
+            message = ' '.join(request[6:])
+        elif is_pinned and not color:
+            message = ' '.join(request[5:-1])
+        else:
+            message = ' '.join(request[5:])
+        color = DEFAULT_COLOR if not color else color
+
+        # Create note in dictionary
+        note = {'x':x, 'y':y, 'width':width, 'height':height, 'color':color.upper(), 'message':message, 'is_pinned':is_pinned}
+        print('note:',note)
+        notes.append(note)
+        response += 'Note added to the board successfully.'
+    except Exception as e:
+        response = e
+    return response, notes
+
+
+# Takes in request, GET required notes based on request params
+def server_get(request, notes):
+    response = ''
+    # condition 0 GET ALL
+    if request[1].upper() == 'ALL':
+        response = "All notes:\n"
+        for note in notes:
+            response += '{},{} - {}\n'.format(note['x'],note['y'],note['message'])
+    # condition 1 GET PINS
+    elif request[1].upper() == 'PINS':
+        response += "All pins locations:\n"
+        for note in notes:
+            if note['is_pinned'] == True:
+                response += str(note['x'])+','+str(note['y'])+'\n'
+    # Condition 2 GET color=red contains=4,6 refersTo=Fred
+    else:
+        # Loop through each request parameter after GET
+        filtered_notes = []
+        is_filtered = False
+        for item in request[1:]:
+            print(item)
+            # item without '=' is invalid
+            if '=' not in item:
+                response += 'Invalid pair: {}, skiping to next'.format(item)
+                continue
+            param, value = enumerate(item.split('='))
+            param,value = param[1].upper(), value[1].upper()
+            print(param,value)
+            # COLOR
+            if param == GET_REQ_PARAMS[0]:
+                filtered_notes = list(filter(lambda n: n['color']==value, filtered_notes))
+                is_filtered = True
+            # CONTAINS
+            elif param == GET_REQ_PARAMS[1]:
+                pos = value.split(',')
+                print(pos)
+                if len(pos)!=2:
+                    response = 'Invalid coordinates/formatting. Please retry and follow format x,y (no space).'
+                    break
+                x,y = int(pos[0]), int(pos[1])
+                filtered_notes = list(filter(lambda n: n['x']==x and n['y']==y, filtered_notes))
+                is_filtered = True
+            # REFERSTO
+            elif param == GET_REQ_PARAMS[2]:
+                filtered_notes = list(filter(lambda n: value in n['message'].upper(), filtered_notes))
+                is_filtered = True
+            # Invalid param
+            else:
+                response = 'Invalid GET parameter: {}\n'.format(item)
+                return response
+            # If after any item search the filtered_notes is empty, exit loop
+            if is_filtered and filtered_notes == []:
+                break
+        response += 'Notes that satify criteria {}:\n'.format(request[1:])
+        if is_filtered and len(filtered_notes) > 0:
+            for n in filtered_notes:
+                response += '{},{} - {}\n'.format(n['x'], n['y'], n['message'])
+        else:
+            response += 'No qualified notes found.'
+    return response
+
 def socket_service(connection_socket):
     global notes
 
@@ -17,134 +164,19 @@ def socket_service(connection_socket):
         print(request)
 
         if request_code == 'CONNECT':
-            response = 'Available colors for notes: ' + \
-                str(avail_colors) + '\nBoard size: ' + \
-                str((board_width, board_height))
-
+            response=server_connect()
         elif request_code == 'DISCONNECT':
             connection_socket.close()
             break
-        
         elif request_code == 'CLEAR':
-            init_len = len(notes)
-            notes = list(filter(lambda n: n['is_pinned']==True, notes))
-            response = "{} unpinned note(s) without removed".format(init_len-len(notes))
-
+            response, notes = server_clear(notes)
         elif request_code in ('PIN','UNPIN'):
-            pin_count = 0
-            response = ''
-            if len(request)==2 and ',' in request[1]:
-                coords = request[1].split(',')
-                x,y = int(coords[0]), int(coords[1])
-                for note in notes:
-                    if note['x']==x and note['y']==y:
-                        if request_code == 'PIN':
-                            if note['is_pinned'] == True:
-                                response += 'Note at {},{} already pinned'.format(x,y)
-                            else:
-                                note['is_pinned'] = True
-                                pin_count+=1
-                        else:
-                            if note['is_pinned'] == False:
-                                response += 'Note at {},{} already not pinned'.format(x,y)
-                            else:
-                                note['is_pinned'] = False
-                                pin_count+=1
-                response += '{} note(s) {}.'.format(pin_count, 'pinned' if request_code=='PIN' else 'unpinned')
-
+            response = server_pin_unpin(request, request_code)
         elif request_code == 'POST':
-            response = ''
-            try:
-                x = int(request[1])
-                y = int(request[2])
-                width = int(request[3])
-                height = int(request[4])
-                if x<0 or y<0 or y>board_height or width > board_width or height>board_height:
-                    raise Exception("Note does not fit on the board. Please resize/re-position to fit it in {}x{}.".format(board_width, board_height))
-
-                color = request[5]
-                # If the value at index 5 is not in available color list, set it to None
-                if color.upper() not in avail_colors:
-                    color = None
-                # If status is not pinned, set is_pinned to False
-                status = request[-1].upper()
-                is_pinned = True if status == 'PINNED' else False
-
-                if color and is_pinned:
-                    message = ' '.join(request[6:-1])
-                elif color and not is_pinned:
-                    message = ' '.join(request[6:])
-                elif is_pinned and not color:
-                    message = ' '.join(request[5:-1])
-                else:
-                    message = ' '.join(request[5:])
-                color = default_color if not color else color
-
-                # Create note in dictionary
-                note = {'x':x, 'y':y, 'width':width, 'height':height, 'color':color.upper(), 'message':message, 'is_pinned':is_pinned}
-                print('note:',note)
-                notes.append(note)
-                response += 'Note added to the board successfully.'
-            except Exception as e:
-                response = e
-
+            response, notes = server_post(request, notes)
         elif request_code == 'GET':
-            response = ''
-            # condition 0 GET ALL
-            if request[1].upper() == 'ALL':
-                response = "All notes:\n"
-                for note in notes:
-                    response += '{},{} - {}\n'.format(note['x'],note['y'],note['message'])
-
-            # condition 1 GET PINS
-            elif request[1].upper() == 'PINS':
-                response += "All pins locations:\n"
-                for note in notes:
-                    if note['is_pinned'] == True:
-                        response += str(note['x'])+','+str(note['y'])+'\n'
-
-            # Condition 2 GET color=red contains=4,6 refersTo=Fred
-            else:
-                # Loop through each request parameter after GET
-                filtered_notes = notes
-                for item in request[1:]:
-                    print(item)
-                    # item without '=' is invalid
-                    if '=' not in item:
-                        response += 'Invalid pair: {}, skiping to next'.format(item)
-                        continue
-                    param, value = enumerate(item.split('='))
-                    param,value = param[1].upper(), value[1].upper()
-                    print(param,value)
-                    # COLOR
-                    if param == GET_REQ_PARAMS[0]:
-                        filtered_notes = list(filter(lambda n: n['color']==value, filtered_notes))
-                    # CONTAINS
-                    elif param == GET_REQ_PARAMS[1]:
-                        pos = value.split(',')
-                        print(pos)
-                        if len(pos)!=2:
-                            response = 'Invalid coordinates/formatting. Please retry and follow format x,y (no space).'
-                            break
-                        x,y = int(pos[0]), int(pos[1])
-                        filtered_notes = list(filter(lambda n: n['x']==x and n['y']==y, filtered_notes))
-                    # REFERSTO
-                    elif param == GET_REQ_PARAMS[2]:
-                        filtered_notes = list(filter(lambda n: value in n['message'].upper(), filtered_notes))
-                    # Invalid param
-                    else:
-                        response = 'Invalid GET parameter: {}\n'.format(item)
-                    # If after any item search the filtered_notes is empty, exit loop
-                    if filtered_notes == []:
-                        break
-                response += 'Notes that satify above criteria {}:\n'.format(request[1:])
-                if filtered_notes == []:
-                    response += 'None'
-                else:
-                    for n in filtered_notes:
-                        response += '{},{} {}\n'.format(n['x'], n['y'], n['message'])
-                print('response:',response)
-      
+            response = server_get(request, notes)
+        # Sent the response message to client
         connection_socket.send(response.encode())
 
 
@@ -155,13 +187,12 @@ if __name__ == "__main__":
         # User inputs
         sys.argv
         server_port = int(input('Enter an available port number: '))
-        board_width = int(input('Enter note board width: '))
-        board_height = int(input('Enter note board height: '))
-        avail_colors = input(
+        BOARD_WIDTH = int(input('Enter note board width: '))
+        BOARD_HEIGHT = int(input('Enter note board height: '))
+        AVAIL_COLORS = input(
             'Enter note color options separated by space (first input as default colour): ').upper().split()
-        default_color = avail_colors[0]
-
-
+        DEFAULT_COLOR = AVAIL_COLORS[0]
+        #This note will be global among all threads
         notes = []
         # Create a TCP server socket
         server_socket = socket(AF_INET, SOCK_STREAM)
